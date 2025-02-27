bac5ed15b4d76eda3783f62ec5dfd91ea6370858
ptckznn1 <PT74866240@idat.pe>
2025-02-27 08:52:32 -0500

    initial proyect

---
+++ src/test/java/pe/edu/idat/demo_metodos_rest/DemoMetodosRestApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_metodos_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMetodosRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
