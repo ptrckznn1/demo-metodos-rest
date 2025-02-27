31f25acfad764bcdad430a2911876553ee7a9d25
ptckznn1 <PT74866240@idat.pe>
2025-02-27 11:27:31 -0500

    1

--- src/main/java/pe/edu/idat/demo_metodos_rest/Ejercicio1Controller.java
+++ src/main/java/pe/edu/idat/demo_metodos_rest/Ejercicio1Controller.java
@@ -1,4 +1,12 @@
 package pe.edu.idat.demo_metodos_rest;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Ejercicio1Controller {
+    private final Ejercicio1Service ejercicio1Service;
+
+    public Ejercicio1Controller(Ejercicio1Service ejercicio1Service){
+        this.ejercicio1Service=ejercicio1Service;
+    }
 }
--- src/main/java/pe/edu/idat/demo_metodos_rest/Ejercicio1Service.java
+++ src/main/java/pe/edu/idat/demo_metodos_rest/Ejercicio1Service.java
@@ -1,4 +1,11 @@
 package pe.edu.idat.demo_metodos_rest;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class Ejercicio1Service {
+
+    public String SaludoEjercicio(){
+        return "";
+    }
 }
